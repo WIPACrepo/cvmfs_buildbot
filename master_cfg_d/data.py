45794bcbe8db7db10d367398bea8392a6b723727
David Schultz <davids24@gmail.com>
2019-04-05 14:04:14 -0500

    add rsync for i3-test-data

--- master_cfg_d/data.py
+++ master_cfg_d/data.py
@@ -20,7 +20,8 @@ def setup(cfg):
     ####### CHANGESOURCES
 
     ####### BUILDERS
-    
+
+    # GCD sync
     build_factory = util.BuildFactory()
     build_factory.addStep(steps.ShellCommand(
         name='copy GCD',
@@ -38,7 +39,6 @@ def setup(cfg):
         }
     ))
 
-
     cfg['builders']['gcd_builder'] = util.BuilderConfig(
         name='gcd_builder',
         workername='cvmfs-centos7-build',
@@ -46,10 +46,41 @@ def setup(cfg):
         properties={},
     )
 
+    # test-data sync
+    build_factory = util.BuildFactory()
+    build_factory.addStep(steps.ShellCommand(
+        name='copy test-data',
+        command=['rsync','-vrlpt','--delete','code.icecube.wisc.edu::Offline/test-data/releases/V00-00-01/','/cvmfs/icecube.opensciencegrid.org/data/i3-test-data/'],
+        workdir='/cvmfs/icecube.opensciencegrid.org/data',
+        haltOnFailure=True,
+        locks=[
+            cfg.locks['cvmfs_shared'].access('counting')
+        ],
+    ))
+    build_factory.addStep(steps.Trigger(schedulerNames=['publish-trigger'],
+        waitForFinish=True,
+        set_properties={
+            'variant': 'data/i3-test-data',
+        }
+    ))
+
+    cfg['builders']['test_data_builder'] = util.BuilderConfig(
+        name='test_data_builder',
+        workername='cvmfs-centos7-build',
+        factory=build_factory,
+        properties={},
+    )
+
+
     ####### SCHEDULERS
-    cfg['schedulers'][prefix+'-force'] = schedulers.ForceScheduler(
-        name=prefix+"-force",
+    cfg['schedulers'][prefix+'-gcd-force'] = schedulers.ForceScheduler(
+        name=prefix+"-gcd-force",
         builderNames=['gcd_builder'],
+        properties=[],
+    )
+    cfg['schedulers'][prefix+'-test-data-force'] = schedulers.ForceScheduler(
+        name=prefix+"-test-data-force",
+        builderNames=['test_data_builder'],
         properties=[],
     )
 
